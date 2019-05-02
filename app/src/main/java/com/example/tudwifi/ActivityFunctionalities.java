8cba31ff8a39d6e722150135d033e0872ec046bb
Shikhar <shikhar@wlan-145-94-183-91.wlan.tudelft.nl>
2019-05-02 19:13:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tudwifi/ActivityFunctionalities.java
@@ -0,0 +1,4 @@
+package com.example.tudwifi;
+
+public class ActivityFunctionalities {
+}
---
+++ app/src/main/java/com/example/tudwifi/LocalizationFunctionalities.java
@@ -0,0 +1,13 @@
+package com.example.tudwifi;
+
+public class LocalizationFunctionalities {
+
+    // TODO: check if the training table exists
+    public boolean isTestReady () {
+        return true;
+    }
+
+    public   String getCurrentLocation () {
+        return "You are still developing!";
+    }
+}
